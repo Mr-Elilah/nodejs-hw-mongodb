98d9318ec1a7471f00da7865a330e1a388154275
Mr.Elijah <beztalannyj.ilia@gmail.com>
2025-05-24 15:23:41 +0200

    fixing-proces-g

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,7 +6,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string()
     .pattern(/^\+?[0-9\s\-()]{7,20}$/)
     .required(),
-  favorite: Joi.boolean(),
+  favourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
@@ -14,6 +14,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
   phoneNumber: Joi.string().pattern(/^\+?[0-9\s\-()]{7,20}$/),
-  favorite: Joi.boolean(),
+  favourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 }).min(1);
